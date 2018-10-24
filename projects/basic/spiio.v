690b825c1953c7f634d53f446505556da54936fd
pdaxrom <sashz@pdaxrom.org>
2018-10-24 17:20:00 +0400

    spi 8/16 bit port

--- projects/basic/spiio.v
+++ projects/basic/spiio.v
@@ -1,11 +1,14 @@
 /*
 	SD card interface
 	
-	$0    RW - RDY|XXX|XXX|XXX|XXX|XXX|XXX|SS0
-	$1    RW - DATA
-	$2    RW - PRESCALER
-	RDY - R- IO ready
-	SS0 - select SPI device 0
+	$0    RW - DATA HIGH BYTE
+	$1    RW - DATA LOW BYTE
+	$2    RW - RDY|XXX|SSM|16B|XXX|XXX|XXX|SS0
+	$3    RW - PRESCALER
+	RDY - R- - IO ready
+	SSM - RW - SS0 manual control
+	16B - RW - 16 bit mode
+	SS0 - RW - select SPI device 0
 */
 module spiio (
 	input wire clk,
@@ -22,81 +25,104 @@ module spiio (
 	output wire mosi,
 	output reg msck,
 	input wire miso,
-	output reg mss
+	output wire mss
 );
-	reg [7:0] spics_reg;
-	reg [7:0] config_reg;
-	reg [7:0] rx_data;
-	reg [7:0] tx_data;
+	reg cfg_ssm;
+	reg cfg_16b;
+	reg cfg_ss0;
+	reg int_mss;
+
+	reg [15:0] rx_data;
+	reg [15:0] tx_data;
 	reg [7:0] prescaler;
 	
-	reg start;
+	wire start;
+	reg  start_hi;
+	reg  start_lo;
 	
-	reg [7:0] shifted_tx_data;
-	reg [3:0] bit_counter;
+	reg [15:0] shifted_tx_data;
+	reg [4:0] bit_counter;
 	reg [7:0] scale_counter;
+	
 	wire data_ready = ((bit_counter == 0) && (!msck))?1'b1:1'b0;
 
 	always @ (posedge clk) begin
 		if (rst) begin
-			config_reg <= 8'b00000001;
-			tx_data <= 8'b11111111;
+			cfg_ssm <= 0;
+			cfg_16b <= 0;
+			cfg_ss0 <= 1;
+			tx_data <= 16'b1111111111111111;
 			prescaler <= 0;
-			start <= 0;
+			start_hi <= 0;
+			start_lo <= 0;
 		end else begin
 			if (cs) begin
 				if (rw) begin
 					case (AD[2:0])
-					3'b000: DO <= {data_ready, 3'b0, config_reg[3:0]};
-					3'b001: begin
-							DO <= rx_data;
-						end
-					3'b010: DO <= prescaler;
-					3'b011: DO <= spics_reg;
+					3'b000: DO <= rx_data[15:8];
+					3'b001: DO <= rx_data[7:0];
+					3'b010: DO <= {data_ready, 1'b0, cfg_ssm, cfg_16b, 1'b0, 1'b0, 1'b0, cfg_ss0};
+					3'b011: DO <= prescaler;
 					endcase
 				end else begin
 					case (AD[2:0])
-					3'b000: config_reg <= DI;
-					3'b001: begin
-							tx_data <= DI;
-							start <= 1'b1;
+					3'b000: begin
+							tx_data[15:8] <= DI;
+							start_hi <= 1'b1;
 						end
-					3'b010: prescaler <= DI;
-					3'b011: spics_reg <= DI;
+					3'b001: begin
+							tx_data[7:0] <= DI;
+							start_lo <= 1'b1;
+						end
+					3'b010: begin
+							cfg_ssm <= DI[5];
+							cfg_16b <= DI[4];
+							cfg_ss0 <= DI[0] & (cfg_ssm | DI[5]);
+						end
+					3'b011: prescaler <= DI;
 					endcase
 				end
 			end else begin
-				if (!data_ready) start <= 1'b0;
+				if (!data_ready) begin
+					start_hi <= 1'b0;
+					start_lo <= 1'b0;
+				end
 			end
 		end
 	end
 
-	assign mosi = ((bit_counter == 0) && (!msck))?1'b1:shifted_tx_data[7];
+	assign start = (cfg_16b ? start_hi : 1'b1) & start_lo;
+
+	assign mss = cfg_ssm ? cfg_ss0 : int_mss;
+
+	assign mosi = ((bit_counter == 0) && (!msck)) ? 1'b1 :
+					cfg_16b ? shifted_tx_data[15] :
+					shifted_tx_data[7];
 
 	always @ (posedge clk_in) begin
 		if (rst) begin
 			msck <= 0;
-			mss <= 0;
-			rx_data <= 8'b11111111;
+			int_mss <= 1'b1;
+			rx_data <= 16'b1111111111111111;
 			scale_counter <= 0;
 		end else if (start) begin
 			shifted_tx_data <= tx_data;
-			bit_counter <= 8;
-			mss <= 1'b0;
+			bit_counter <= cfg_16b ? 16 : 8;
+			int_mss <= 1'b0;
 		end else begin
 			if (bit_counter != 0) begin
 				if (scale_counter == prescaler) begin
 					scale_counter <= 0;
 					msck <= ~msck;
 					if (msck) begin
-						shifted_tx_data <= {shifted_tx_data[6:0], 1'b1};
-						rx_data <= {rx_data[6:0], miso};
+						shifted_tx_data <= cfg_16b ? {shifted_tx_data[14:0], 1'b1} : {shifted_tx_data[6:0], 1'b1};
+						rx_data <= cfg_16b ? {rx_data[14:0], miso} : {rx_data[6:0], miso};
 						bit_counter <= bit_counter - 1'b1;
 					end
 				end else scale_counter <= scale_counter + 1'b1;
 			end else begin
 				msck <= 0;
-				mss <= 1'b1;
+				int_mss <= 1'b1;
 			end
 		end
 	end
